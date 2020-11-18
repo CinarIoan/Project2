9ff29b6fc27ee1dcf858656af5554e868797a911
CinarIoan <cinar.ioan@gmail.com>
2020-11-18 23:26:04 +0200

    testCommit

---
+++ Project2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nume: ");
+            string nume = Console.ReadLine();
+            nume = nume.ToUpper();
+            Console.Write("prenume: ");
+            string prenume = Console.ReadLine().ToUpperInvariant();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my first Console Project in Ubuntu");
+            Console.WriteLine($"This is {nume} {prenume}");
+            Console.ReadLine();
+        }
+    }
+}
